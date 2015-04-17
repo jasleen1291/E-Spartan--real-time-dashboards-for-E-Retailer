7c287a5ecf6a53da4a4ecf7524cae06e7e4f7b80
jasleen <jasleenkaur1291@gmail.com>
2015-04-17 15:43:23 -0700

    Fixed the bug

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ var router=express.Router();
 
 var userRoutes=require('./routes/users')(router);
 app.use('/api',router);
-app.listen(port);
-<<<<<<< HEAD
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -31,7 +29,7 @@ var mongoose=require('mongoose');
 
 mongoose.createConnection('mongodb://cmpe280:cmpe280@ds061651.mongolab.com:61651/cmpe280');
 
-var port = 8080;
+var port = 80;
 
 //Routes for APIS
 
@@ -41,5 +39,3 @@ var userRoutes=require('./routes/users')(router);
 app.use('/api',router);
 app.listen(port);
 
-=======
->>>>>>> 3b62b86a5938d17651c97f37d4887ac7dc8827a1
