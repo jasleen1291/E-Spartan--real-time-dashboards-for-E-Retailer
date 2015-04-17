ba924f169653a00cfebd3b22daee2ac42ee0c8d1
jasleen <jasleenkaur1291@gmail.com>
2015-04-17 15:20:41 -0700

    Just cause

--- app.js
+++ app.js
@@ -13,13 +13,30 @@ var port = 80;
 
 //Routes for APIS
 
-<<<<<<< HEAD
 var router=express.Router();
 
 var userRoutes=require('./routes/users')(router);
 app.use('/api',router);
 app.listen(port);
-=======
-//Testing from Tania
->>>>>>> b39011200d3f337ea21f300c5162666edc587012
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var mongoose=require('mongoose');
+
+mongoose.createConnection('mongodb://cmpe280:cmpe280@ds061651.mongolab.com:61651/cmpe280');
+
+var port = 8080;
+
+//Routes for APIS
+
+var router=express.Router();
+
+var userRoutes=require('./routes/users')(router);
+app.use('/api',router);
+app.listen(port);
 
--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,50 @@
+module.exports = function(router) {
+    var Counters = require('../app/models/counters');
 
-module.exports=function(router)
-{	
+    function getNextSequence(name) {
+        // get the user starlord55
+        console.log(name);
+    }
+    var UserSchema = require('../app/models/user/user');
+    router.post('/signup', function(req, res, next) {
+        var UserSchema = require('../app/models/user/user');
 
+        UserSchema.findOne({
+            username: req.body.userName,
+            password: req.body.password
+        }, function(err, user) {
+            if (err) {
+                console.log('err');
+                res.json({
+                    type: false,
+                    data: "Error occured: " + err
+                });
+            } else {
+                if (user) {
+                    console.log('exists');
+                    res.json({
+                        type: false,
+                        data: "User already exists!"
+                    });
+                } else {
+                    console.log('new');
+                    var userModel = new UserSchema();
+                    console.log("data: " + req.body.userName);
+                    userModel.username = req.body.userName;
+                    userModel.password = req.body.password;
+                    userModel.firstname = req.body.firstName;
+                    userModel.lastname = req.body.lastName;
+                    getNextSequence("user");
+                    userModel.save(function(err, user) {
+                        res.json({
+                            type: true,
+                            data: user
+                        });
+                    })
+                }
+            }
+        });
+    });
+    
 
-	router.post('/signup',function(req, res, next) {
-		var UserSchema=require('../app/models/user/user');
-
-		UserSchema.findOne({username: req.body.userName, password: req.body.password}, function(err, user) {
-		if (err) {
-			console.log('err');
-			res.json({
-				type: false,
-				data: "Error occured: " + err
-			});
-		} else {
-			if (user) {
-				console.log('exists');
-				res.json({
-					type: false,
-					data: "User already exists!"
-				});
-			} else {
-				console.log('new');
-				var userModel = new UserSchema();
-				console.log("data: " + req.body.userName);
-				userModel.username = req.body.userName;
-				userModel.password = req.body.password;
-				userModel.firstname = req.body.firstName;
-				userModel.lastname = req.body.lastName;
-				userModel.instances = [];
-				userModel.save(function(err, user) {
-						res.json(user);
-				})
-			}
-		}
-	});
-	});
 }
