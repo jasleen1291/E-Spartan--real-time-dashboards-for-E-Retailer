6020d847e23a7615b4495a2d40bf60345ab9a1da
jasleen <jasleenkaur1291@gmail.com>
2015-04-17 16:09:12 -0700

    Added created at and updated at fields

--- app/models/user/user.js
+++ app/models/user/user.js
@@ -10,6 +10,19 @@ var userSchema= new Schema(
 	created_at:Date,
 	updated_at:Date
 });
+userSchema.pre('save', function(next) {
+  // get the current date
+  var currentDate = new Date();
+  
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
+});
 userSchema.plugin(autoIncrement.plugin, 'User');
 var User=mongoose.model('User',userSchema);
 module.exports=User;
