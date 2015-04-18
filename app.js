9cb1210d0575cb679ef496c66f0fe08a775d3357
Piyush Bansal <piyush043@gmail.com>
2015-04-17 17:40:27 -0700

    removed duplicate requires from app.js

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
 var express = require('express');
+var path = require('path');
 var bodyParser = require('body-parser');
-var app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
 var mongoose=require('mongoose');
-var autoIncrement = require('mongoose-auto-increment');
-var connection=mongoose.connect('mongodb://cmpe280:cmpe280@ds061651.mongolab.com:61651/cmpe280');
-autoIncrement.initialize(connection);
-var port = 80;
-
-//Routes for APIS
-
-var router=express.Router();
-
-var userRoutes=require('./routes/users')(router);
-app.use('/api',router);
-
 var express = require('express');
 var bodyParser = require('body-parser');
-var app = express();
+var autoIncrement = require('mongoose-auto-increment');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+var connection=mongoose.connect('mongodb://cmpe280:cmpe280@ds061651.mongolab.com:61651/cmpe280');
+autoIncrement.initialize(connection);
 
-var mongoose=require('mongoose');
-
-mongoose.createConnection('mongodb://cmpe280:cmpe280@ds061651.mongolab.com:61651/cmpe280');
-
-var port = 80;
+require('./models/User');
+require('./models/Item');
+require('./models/Category');
+require('./models/Rating');
 
 //Routes for APIS
 
 var router=express.Router();
-
 var userRoutes=require('./routes/users')(router);
+
+var app = express();
+var port = 80;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use('/api',router);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//mongoose.createConnection('mongodb://cmpe280:cmpe280@ds061651.mongolab.com:61651/cmpe280');
+
+//Routes for APIS
+
 app.listen(port);
 
