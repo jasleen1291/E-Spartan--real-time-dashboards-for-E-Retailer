73c649d860851c45ef8d238dbed5a61b8deb4a69
Piyush Bansal <piyush043@gmail.com>
2015-04-23 15:00:44 -0700

    Dashboard working - deleting unwanted files

--- routes/rate.js
+++ routes/rate.js
@@ -33,9 +33,7 @@ module.exports = function(router) {
                             data: rating
                         });
                     })
-
                 }
-
             }
         });
     });
